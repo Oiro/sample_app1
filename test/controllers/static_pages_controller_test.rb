45f34b7d4d7cb6a907c7765ec7989df07cfa7b4c
Oiro <moses.oiro.okello@gmail.com>
2016-10-21 18:12:23 +0300

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,31 @@
-require 'test_helper'
+require'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
+    # get :home
+    # get '/home'
+    # get 'static_pages/home'
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
+    # get :help
+    # get '/help'
+    # get 'static_pages/help'
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+	get static_pages_about_url
+	# get :about
+	# get '/about'
+	# get 'static_pages/about'
+	assert_response :success
+	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
