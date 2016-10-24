34f8b6ffc9fbb2af7e5ff4a0b6ad6d5ef5d0e6dd
Oiro <moses.oiro.okello@gmail.com>
2016-10-24 08:23:43 +0300

    added setup and base title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,31 +1,33 @@
 require'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+  @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     # get :home
-    # get '/home'
-    # get 'static_pages/home'
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    # assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     # get :help
-    # get '/help'
-    # get 'static_pages/help'
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    # assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
 	get static_pages_about_url
 	# get :about
-	# get '/about'
-	# get 'static_pages/about'
 	assert_response :success
-	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+	# assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  assert_select "title", "About | #{@base_title}"
   end
 
 end
