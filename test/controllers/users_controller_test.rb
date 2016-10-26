475ecb3c6e2837d8fec6bc7622d9a087c6167b47
Oiro <moses.oiro.okello@gmail.com>
2016-10-26 22:59:22 +0300

    finish layout and routes

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  test "should get new" do
+    # get users_new_url
+    get :new
+    assert_response :success
+  end
+
+end
