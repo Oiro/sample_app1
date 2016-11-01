796d896ce0dac21e8a7306dd5396e44be6f2d864
Oiro <moses.oiro.okello@gmail.com>
2016-11-01 13:02:16 +0300

    configuring smtp to send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app1_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: "www.example.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "example.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["mokeroke"],
+  password: ENV["bounty"]
+}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
