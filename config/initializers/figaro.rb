9b9f6b4606a3b4ce97e533b47c87b003044cd03e
Oiro <moses.oiro.okello@gmail.com>
2016-11-01 15:58:53 +0300

    configuring smtp to send mail

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,1 +1,2 @@
-Figaro.require_keys('mokeroke@gmail.com', 'bounty')  
+# Figaro.require_keys('mokeroke@gmail.com', 'bounty')  
+Figaro.require_keys("2954", "7381a978f7dd7f9a1117", "abdc3b896a0ffb85d373")
