f2a1423a108ad284aabb2a2b536db1ba9802ab95
Oiro <moses.oiro.okello@gmail.com>
2016-11-01 13:06:47 +0300

    configuring smtp to send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => "587",
-    :domain => "mydomain.net",
+    :domain => "example.com",
     :user_name => "mokeroke@gmail.com",
     :password => "bounty",
     :authentication => "plain",
