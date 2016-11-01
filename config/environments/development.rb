99ba8e39bf93b36959bb9c5963128c5051e61220
Oiro <moses.oiro.okello@gmail.com>
2016-11-01 15:11:20 +0300

    configuring smtp to send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,13 +41,12 @@ Rails.application.configure do
   # host = 'localhost:3000'
   # config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "example.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["mokeroke"],
-  password: ENV["bounty"]
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :user_name            => ENV['mokeroke@gmail.com'],
+        :password             => ENV['bounty'],
+        :authentication       => "plain",
+        :enable_starttls_auto => true
 }
   config.action_mailer.perform_caching = false
 
--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,9 +1,11 @@
 ActionMailer::Base.smtp_settings = {
-    address: "smtp.gmail.com",
-  	port: 587,
-  	domain: "example.com",
-  	authentication: "plain",
-  	enable_starttls_auto: true,
-  	user_name: ENV["mokeroke"],
-  	password: ENV["bounty"]
+    
+  	
+  	  :domain 				=> "example.com",
+  	  :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => ENV['mokeroke@gmail.com'],
+      :password             => ENV['bounty'],
+      :authentication       => "plain",
+      :enable_starttls_auto => true
 }
