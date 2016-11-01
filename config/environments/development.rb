1b452ee65dcdc1a29dcda1a72fc365cda329cc6e
Oiro <moses.oiro.okello@gmail.com>
2016-11-01 13:17:14 +0300

    configuring smtp to send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,17 +41,15 @@ Rails.application.configure do
   # host = 'localhost:3000'
   # config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => "587",
-    :domain => "example.com",
-    :user_name => "mokeroke@gmail.com",
-    :password => "bounty",
-    :authentication => "plain",
-    :enable_starttls_auto => true
-
-  }
-
-  config.action_mailer.perform_caching = true
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "example.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["mokeroke"],
+  password: ENV["bounty"]
+}
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
