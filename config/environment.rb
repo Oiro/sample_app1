b4803c3764cee5e9d09b3491f858d462e2532713
Oiro <moses.oiro.okello@gmail.com>
2016-11-03 14:31:26 +0300

    configuring smtp to send mail

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,14 @@ Rails.application.initialize!
 
 
   # config.gem "ambethia-smtp-tls", :lib =&gt; "smtp-tls", :source =&gt; "http://gems.github.com/"
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.server_settings = {
+    :address        => 'smtp.gmail.com',
+    :domain         => '<your domain>',
+    :port           => 587,
+    :user_name      => '<your gmail>',
+    :password       => '<your password>',
+    :authentication => :plain
+}
