ab3994f5d4af388eb628e751dd3f641ae80b106f
Oiro <moses.oiro.okello@gmail.com>
2016-11-03 13:35:04 +0300

    add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 # class User < ApplicationRecord
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship",
+									foreign_key: "follower_id",
+				 					dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+									foreign_key: "followed_id",
+									dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 	# before_save { self.email = email.downcase }
 	attr_accessor :remember_token, :activation_token, :reset_token
 	# before_save { email.downcase! }
@@ -10,8 +18,8 @@ class User < ActiveRecord::Base
 	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
-							format: { with: VALID_EMAIL_REGEX },
-							uniqueness: { case_sensitive: false }
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 	has_secure_password
 	# validates :password, length: { minimum: 6 }
 	validates :password, length: { minimum: 6 }, allow_blank: true
@@ -76,7 +84,25 @@ class User < ActiveRecord::Base
 	# Defines a proto-feed.
 	# See "Following users" for the full implementation.
 	def feed
-		Micropost.where("user_id = ?", id)
+		following_ids = "SELECT followed_id FROM relationships
+										WHERE follower_id = :user_id"
+		Micropost.where("user_id IN (#{following_ids})
+								OR user_id = :user_id", user_id: id)
+	end
+
+	# Follows a user.
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+
+	# Unfollows a user.
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	# Returns true if the current user is following the other user.
+	def following?(other_user)
+		following.include?(other_user)
 	end
 
 	private
